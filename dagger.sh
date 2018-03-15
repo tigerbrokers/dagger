610c319493a246b024fa1291124afb3c09509d90
Li Yi <i@ijser.cn>
2018-03-15 13:25:21 +0800

    set work dir

--- dagger.sh
+++ dagger.sh
@@ -1,10 +1,12 @@
 #/bin/sh
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # 16位secret，在运维发的二维码链接里面找
 secret=$DAGGER_GAUTH_SECRET
 
 # 获得google authenticator token, token只能使用一次, 30秒更新一次
-code=`python google_token.py $secret`
+code=`python $DIR/google_token.py $secret`
 
 # 跳板机用户名SSH
 ssh_login=$DAGGER_SSH_LOGIN
@@ -12,5 +14,5 @@ ssh_login=$DAGGER_SSH_LOGIN
 password=$DAGGER_SSH_PASSWORD
 
 # 自动登录
-expect ./login_pro.exp $ssh_login $code "$password"
+expect $DIR/login_pro.exp $ssh_login $code "$password"
 
